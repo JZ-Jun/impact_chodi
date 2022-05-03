f96d7d1bb5e650646001b2d963e6dd71f1ca6814
JamesDBUCI <44314052+JamesDBUCI@users.noreply.github.com>
2022-05-03 00:18:35 -0700

    Changed Performance Widget to not be const
    bad break if it is

--- chodi_message-2022/lib/screens/impact/impact_screen.dart
+++ chodi_message-2022/lib/screens/impact/impact_screen.dart
@@ -436,7 +436,7 @@ class _ImpactScreenState extends State<ImpactScreen>
                                   onTap: () {
                                     Provider.of<MainScreenViewModel>(this.context,
                                         listen: false)
-                                        .setWidget(const PerformanceScreen());
+                                        .setWidget(PerformanceScreen());
                                   },
                                   child: const Text(
                                     "See Performance",
